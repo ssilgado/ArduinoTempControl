2801d632cfe1546249ddcd5fe344b0148dac9cb8
Steven Silgado <steven.silgado@outlook.com>
2022-04-24 16:03:11 -0500

    Cleaned up IR logic

--- TempController.IRLogic/src/main.cpp
+++ TempController.IRLogic/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
 #include <ac_LG.hpp>
+#include "main.hpp"
 
 
 #define DECODE_LG
@@ -12,14 +13,18 @@
 #define ON_OFF_BUTTON_PIN 13
 #define HEATER_AC_BUTTON_PIN 12
 
+// Defining the most efficient AC temperatures
+#define HEAT_TEMP 30
+#define COOL_TEMP 18
+
 Aircondition_LG MyLG_Airconditioner;
 
 void setup()
 {
-  // Just to know which program is running
+  // Initialize the serial monitor
   Serial.begin(115200);
 
-  // Initialize the LG Airconditioner
+  // Initialize the LG Airconditioner initial state
   MyLG_Airconditioner.ACIsWallType = true;
   MyLG_Airconditioner.Mode = AC_MODE_COOLING;
   MyLG_Airconditioner.Temperature = 18;
@@ -29,7 +34,7 @@ void setup()
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
 
-  // Initialize buttons
+  // Initialize manual control buttons
   pinMode(ON_OFF_BUTTON_PIN, INPUT);
   pinMode(HEATER_AC_BUTTON_PIN, INPUT);
 }
@@ -39,41 +44,48 @@ void loop()
   // Check if on/off button is pressed and switch the AC state
   if (digitalRead(ON_OFF_BUTTON_PIN) == LOW)
   {
-    bool ACState = MyLG_Airconditioner.PowerIsOn;
-    if (MyLG_Airconditioner.sendCommandAndParameter(ACState ? LG_COMMAND_OFF : LG_COMMAND_ON, 0))
-    {
-      Serial.print("AC state switched to ");
-      Serial.println(ACState ? "OFF" : "ON");
-    }
+    togglePowerState();
     delay(500);
   }
 
   // Check if the AC/Heater button is pressed and switch the AC/Heater mode
   if (digitalRead(HEATER_AC_BUTTON_PIN) == LOW)
   {
-    if (MyLG_Airconditioner.Mode == AC_MODE_COOLING)
+    toggleACMode();
+    delay(500);
+  }
+}
+
+void togglePowerState()
+{
+  bool ACState = MyLG_Airconditioner.PowerIsOn;
+  if (MyLG_Airconditioner.sendCommandAndParameter(ACState ? LG_COMMAND_OFF : LG_COMMAND_ON, 0))
+  {
+    Serial.print("AC state switched to ");
+    Serial.println(ACState ? "OFF" : "ON");
+  }
+}
+
+void toggleACMode()
+{
+  if (MyLG_Airconditioner.Mode == AC_MODE_COOLING)
     {
-      Serial.println(MyLG_Airconditioner.Mode);
-      MyLG_Airconditioner.Temperature = 30;
+      MyLG_Airconditioner.Temperature = HEAT_TEMP;
       if (MyLG_Airconditioner.sendCommandAndParameter(LG_COMMAND_MODE, AC_MODE_HEATING))
       {
-        Serial.println("AC/Heater mode switched to HEATER");
-        MyLG_Airconditioner.Mode = AC_MODE_HEATING;
+        MyLG_Airconditioner.Mode = AC_MODE_HEATING; // Must manually set the mode to HEATING if command is successful
       }
     }
     else if (MyLG_Airconditioner.Mode == AC_MODE_HEATING)
     {
-      Serial.println(MyLG_Airconditioner.Mode);
-      MyLG_Airconditioner.Temperature = 18;
+      MyLG_Airconditioner.Temperature = COOL_TEMP;
       if (MyLG_Airconditioner.sendCommandAndParameter(LG_COMMAND_MODE, AC_MODE_COOLING))
       {
-        Serial.println("AC/Heater mode switched to COOLING");
-        MyLG_Airconditioner.Mode = AC_MODE_COOLING;
+        MyLG_Airconditioner.Mode = AC_MODE_COOLING; // Must manually set the mode to COOLING if command is successful
       }
-    }else{
-      Serial.print("AC/Heater mode is ");
-      Serial.println(MyLG_Airconditioner.Mode);
     }
-    delay(500);
-  }
+
+    // Print the current AC/Heater mode
+    Serial.print("AC/Heater mode is ");
+    Serial.println(MyLG_Airconditioner.Mode == AC_MODE_COOLING ? "COOLING" : "HEATING");
 }
---
+++ TempController.IRLogic/src/main.hpp
@@ -0,0 +1,5 @@
+// Function to toggle the AC power state
+void togglePowerState();
+
+// Function to toggle the AC mode
+void toggleACMode();
